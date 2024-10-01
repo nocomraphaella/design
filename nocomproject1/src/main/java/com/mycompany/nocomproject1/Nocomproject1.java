3b349f372fce223bab830a63e99176c2ec94bdf9
nocomraphaella <160980300+nocomraphaella@users.noreply.github.com>
2024-09-30 19:02:05 -0700

    Add files via upload

---
+++ nocomproject1/src/main/java/com/mycompany/nocomproject1/Nocomproject1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.nocomproject1;
+
+/**
+ *
+ * @author CL3-PC30
+ */
+public class Nocomproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
